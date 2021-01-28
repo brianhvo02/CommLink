7355ecd7ab67d067e640e33f6c94d3f69e36c59b
Brian Vo <brian.vo@sjsu.edu>
2021-01-27 17:48:23 -0800

    incomplete websocket logic

--- CommLink-Server/src/index.ts
+++ CommLink-Server/src/index.ts
@@ -1,17 +1,50 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import * as fs from 'fs/promises';
 import * as https from 'https';
+import * as WebSocket from 'ws';
+import { v4 as uuidv4 } from 'uuid';
+import { serialize, deserialize } from 'bson';
 
 const PORT = 8443;
 
+type Client = {
+    id: string
+}
+type WSClient = WebSocket & Client;
+
 const app = express();
-app.get('/', (req, res) => res.send('Server up!'));
+app.use(cors())
+app.get('/', (req, res) => res.send(true));
 
-app.listen();
+const sendObj = (ws: WebSocket, obj: any) => { ws.send(serialize(obj)); };
 
-Promise.all([ fs.readFile('./keys/localhost.key'), fs.readFile('./keys/localhost.crt') ]).then(([key, cert]) => {
+(async () => {
+    const key = await fs.readFile('./keys/localhost.key');
+    const cert = await fs.readFile('./keys/localhost.crt');
+
     const server = https.createServer({ key, cert }, app);
+
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', (ws: WSClient) => {
+        ws.on('message', message => {
+            
+        });
+
+        ws.on('close', () => {
+            
+        });
+
+        ws.id = uuidv4();
+
+        sendObj(ws, {
+            key: 'test',
+            id: ws.id
+        });
+    });
+    
     server.listen(PORT, () => {
         console.log(`Server is running at https://localhost:${PORT}`);
     });
-});
+})();
--- CommLink-WebClient/src/App.tsx
+++ CommLink-WebClient/src/App.tsx
@@ -1,23 +1,71 @@
 import './App.css';
 import { Component } from 'react';
+import { serialize, deserialize } from 'bson';
 
-class App extends Component {
+const SERVER = 'localhost:8443';
+
+type StateType = {
+	serverOnline: Boolean
+	serverKeysExchanging: Boolean
+	serverKeysExchanged: Boolean
+}
+
+class App extends Component<{}, StateType> {
 	constructor(props: any) {
 		super(props);
 		this.state = {
-
+			serverOnline: false,
+			serverKeysExchanging: false,
+			serverKeysExchanged: false
 		};
 		//this.function = this.function.bind(this);
 	}
 
 	componentDidMount() {
+		fetch(`https://${SERVER}`).then(res => res.text()).then(result => {
+			if (result === 'true') this.setState({
+				serverOnline: true,
+				serverKeysExchanging: true
+			});
+		})
+	}
 
+	componentDidUpdate() {
+		if (this.state.serverOnline && (this.state.serverKeysExchanging && !this.state.serverKeysExchanged)) {
+			const ws = new WebSocket(`wss://${SERVER}`);
+			ws.binaryType = 'arraybuffer';
+			ws.onmessage = e => {
+				console.log(deserialize(e.data));
+			};
+			  
+			ws.onclose = () => {
+				this.setState({
+					serverOnline: false
+				})
+			};
+			  
+		}
 	}
 
 	render() {
 	  	return (
 			<div className="App">
 				<h1>CommLink WebClient</h1>
+				{
+					this.state.serverOnline ? (
+						this.state.serverKeysExchanging ? (
+							this.state.serverKeysExchanged ? (
+								<p></p>
+							) : (
+								<p>Exchanging keys with server...</p>
+							)
+						) : (
+							<p>Error while exchanging keys with server.</p>
+						)
+					) : (
+						<p>Server not online!</p>
+					)
+				}
 			</div>  
 		)
 	}
